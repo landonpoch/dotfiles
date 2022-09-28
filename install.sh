1d3d294c82ebdaa29ac31051eab3be48bebfd99c
landonpoch <landon.poch+git@gmail.com>
2022-09-28 02:54:57 -0600

    Separating out oh-my-zsh install.

--- install.sh
+++ install.sh
@@ -1,9 +1,6 @@
 #!/bin/bash
 set -ex
 
-# Install oh-my-zsh
-sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
-
 # Install brew and software
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 brew bundle
