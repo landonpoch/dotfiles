67772590e8f5b8cee5d9c98c8b583f80802b627b
landonpoch <landon.poch+git@gmail.com>
2022-09-28 03:14:54 -0600

    Updating script to add brew to path.

--- install.sh
+++ install.sh
@@ -3,6 +3,10 @@ set -ex
 
 # Install brew and software
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+echo '# Set PATH, MANPATH, etc., for Homebrew.' >> $HOME/.zprofile
+echo 'eval "$(/opt/homebrew/bin/brew shellenv)"' >> $HOME/.zprofile
+eval "$(/opt/homebrew/bin/brew shellenv)"
+
 brew bundle
 
 # Reload the terminal
